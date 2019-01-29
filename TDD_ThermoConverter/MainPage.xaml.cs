9b5970767f42059e29b382c93d1ec05dbf0fe8bd
Kenichiro Tanaka <kenichiro@future-coders.net>
2019-01-29 13:54:41 +0900

    3-1 ビューとモデルを分離を意識しないアプリの作成

--- TDD_ThermoConverter/MainPage.xaml.cs
+++ TDD_ThermoConverter/MainPage.xaml.cs
@@ -1,30 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
 namespace TDD_ThermoConverter
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender,
+            RangeBaseValueChangedEventArgs e)
+        {
+            Celsius.Text = e.NewValue.ToString();
+            Fahrenheit.Text = (e.NewValue * 1.8 + 32).ToString();
         }
     }
 }
